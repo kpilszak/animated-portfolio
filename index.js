6c5029d3fd9d31e0f45ded118187d83c99232889
kpilszak <klaudia.pilszak@gmail.com>
2023-06-23 17:19:32 -0400

    Add animations to images

--- index.js
+++ index.js
@@ -10,7 +10,29 @@ const graphql = document.getElementById('graphql');
 
 function move() {
     const incrementer = window.scrollY
+    
     balloon.style.bottom = 10 + incrementer * 0.1 + '%';
+
+    cloudOne.style.bottom = 40 + incrementer * 0.12 + '%';
+    cloudOne.style.marginLeft = 70 + incrementer * 0.1 + '%';
+
+    cloudTwo.style.bottom = 80 + incrementer * 0.14 + '%';
+    cloudTwo.style.marginLeft = 60 + incrementer * 0.15 + '%';
+
+    cloudThree.style.bottom = 60 + incrementer * 0.1 + '%';
+    cloudThree.style.marginRight = 80 + incrementer * 0.12 + '%';
+
+    cloudFour.style.bottom = 70 + incrementer * 0.16 + '%';
+    cloudFour.style.marginRight = 40 + incrementer * 0.15 + '%';
+
+    cloudFive.style.bottom = 60 + incrementer * 0.2 + '%';
+    cloudFive.style.marginLeft = 40 + incrementer * 0.16 + '%';
+
+    javascript.style.marginRight = 55 + incrementer * 2.2 + '%';
+    
+    react.style.marginRight = 43 + incrementer * 1.5 + '%';
+    
+    graphql.style.marginRight = 31 + incrementer * 1 + '%';
 }
 
 window.addEventListener('scroll', move)
